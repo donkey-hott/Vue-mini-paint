9ba13f020917a39ee7b2cfebfedd9d077287f961
kochtaik <kochtaik@gmail.com>
2021-05-02 12:46:15 +0300

    restucture router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,52 +1,24 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-
-import Home from "../views/Home.vue";
-import SignUp from "../views/authentication/SignUp.vue";
-import SignIn from "../views/authentication/SignIn.vue";
-import Paint from "../views/Paint.vue";
-
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "./routes";
 import { store } from "../store";
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/sign-up",
-    name: "sign-up",
-    component: SignUp,
-    meta: {
-      isPublic: true,
-    },
-  },
-  {
-    path: "/sign-in",
-    name: "sign-in",
-    component: SignIn,
-    meta: {
-      isPublic: true,
-    },
-  },
-  {
-    path: "/new-canvas",
-    name: "paint",
-    component: Paint,
-  },
-];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _, next) => {
   const isPublic = to.matched.some((record) => record.meta.isPublic);
   const currentUser = store.state.currentUser;
 
   if (!currentUser && !isPublic) {
     return next("/sign-in");
+  }
+
+  /* Prevent authenticated user from entering
+    authentication pages */
+  if (currentUser && isPublic) {
+    return next("/");
   }
   next();
 });
---
+++ src/router/routes.ts
@@ -0,0 +1,35 @@
+import { RouteRecordRaw } from "vue-router";
+
+import Home from "../views/Home.vue";
+import SignUp from "../views/authentication/SignUp.vue";
+import SignIn from "../views/authentication/SignIn.vue";
+import Paint from "../views/Paint.vue";
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/sign-up",
+    name: "sign-up",
+    component: SignUp,
+    meta: {
+      isPublic: true,
+    },
+  },
+  {
+    path: "/sign-in",
+    name: "sign-in",
+    component: SignIn,
+    meta: {
+      isPublic: true,
+    },
+  },
+  {
+    path: "/new-canvas",
+    name: "paint",
+    component: Paint,
+  },
+];
